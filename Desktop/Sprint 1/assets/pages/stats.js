d4ef5fa809363c708674476eb00574a95377b7a6
Daryl Baptie <50216607+DarylBaptie@users.noreply.github.com>
2023-07-17 19:06:49 -0300

    states table filled

--- Desktop/Sprint 1/assets/pages/stats.js
+++ Desktop/Sprint 1/assets/pages/stats.js
@@ -1,12 +1,17 @@
 let events;
 let currentDate;
+let headTable = document.getElementById("headTable");
+let table1 = document.getElementById("firstTable");
+let table2 = document.getElementById("secondTable");
 
 fetch("https://mindhub-xj03.onrender.com/api/amazing")
   .then((res) => res.json())
   .then((data) => {
     events = data.events;
     currentDate = data.currentDate;
-    console.log(revenuePast(events));
+    printRow(revenueFuture(events), table1);
+    printRow(revenuePast(events), table2);
+    printRow2(events, headTable);
   })
   .catch((error) => console.log(error));
 
@@ -20,7 +25,8 @@ function biggestCapacity(array) {
     }
     return 0;
   });
-  return capacitySort;
+
+  return capacitySort[0];
 }
 
 function percentageAssistance(array) {
@@ -28,6 +34,7 @@ function percentageAssistance(array) {
   for (let event of pastEvents) {
     event.percentage = (event.assistance / event.capacity) * 100;
   }
+
   const percentageAssistanceSort = pastEvents.sort((a, b) => {
     if (a.percentage > b.percentage) {
       return -1;
@@ -37,8 +44,11 @@ function percentageAssistance(array) {
     }
     return 0;
   });
+
   return percentageAssistanceSort;
 }
+
+////////////
 
 function revenuePast(array) {
   let pastEvents = array.filter((event) => event.date < currentDate);
@@ -46,14 +56,91 @@ function revenuePast(array) {
     event.revenue = event.assistance * event.price;
   }
 
-  let result = pastEvents.reduce((acc, curr) => {
-    let item = acc.find((event) => event.category === curr.category);
-    if (item) {
-      item.amount += curr.amount;
-    } else {
-      acc.push(curr);
-    }
+  const summed = pastEvents.reduce((acc, cur, i) => {
+    const item = i > 0 && acc.find(({ category }) => category === cur.category);
+    if (item)
+      (item.revenue += cur.revenue),
+        (item.capacity += cur.capacity),
+        (item.assistance += cur.assistance);
+    else
+      acc.push({
+        category: cur.category,
+        revenue: cur.revenue,
+        capacity: cur.capacity,
+        assistance: cur.assistance,
+      }); // don't push cur here
     return acc;
   }, []);
-  return result;
+
+  for (let category of summed) {
+    category.percentileAssistance = (
+      (category.assistance / category.capacity) *
+      100
+    ).toFixed(2);
+  }
+
+  return summed;
+}
+
+function revenueFuture(array) {
+  let pastEvents = array.filter((event) => event.date > currentDate);
+  for (let event of pastEvents) {
+    event.revenue = event.estimate * event.price;
+  }
+  const summed = pastEvents.reduce((acc, cur, i) => {
+    const item = i > 0 && acc.find(({ category }) => category === cur.category);
+    if (item)
+      (item.revenue += cur.revenue),
+        (item.capacity += cur.capacity),
+        (item.estimate += cur.estimate);
+    else
+      acc.push({
+        category: cur.category,
+        revenue: cur.revenue,
+        capacity: cur.capacity,
+        estimate: cur.estimate,
+      }); // don't push cur here
+    return acc;
+  }, []);
+
+  for (let category of summed) {
+    category.percentileAssistance = (
+      (category.estimate / category.capacity) *
+      100
+    ).toFixed(2);
+  }
+
+  return summed;
+}
+
+function createRow(category) {
+  return `
+  <td >${category.category}</td>
+  <td >${category.revenue}</td>
+  <td >${category.percentileAssistance}</td>
+  `;
+}
+
+function printRow(array, location) {
+  for (let checkbox of array) {
+    location.innerHTML += createRow(checkbox);
+  }
+}
+
+function firstRow(array) {
+  let highestEvent = biggestCapacity(array);
+  let highestPercentage = percentageAssistance(array)[0];
+  let lowestPercentage =
+    percentageAssistance(array)[percentageAssistance(array).length - 1];
+  console.log(lowestPercentage);
+  console.log(highestPercentage);
+
+  return `
+  <td>${highestPercentage.name}</td>
+  <td>${lowestPercentage.name}</td>
+  <td>${highestEvent.name}</td>`;
+}
+
+function printRow2(array, location) {
+  location.innerHTML += firstRow(array);
 }
